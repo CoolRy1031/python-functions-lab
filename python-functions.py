93ef258ff5042e5e8e2557b17125b005df6e9cea
CoolRy <that007guy@hotmail.com>
2022-07-31 12:59:15 -0400

    exercise 2

--- python-functions.py
+++ python-functions.py
@@ -1,7 +1,16 @@
-def sum_to(num):
-  sum = 0
-  for n in range(1, num + 1):
-    sum += n
-  return sum
-print(sum_to(6))
-print(sum_to(10))
+# def sum_to(num):
+#   sum = 0
+#   for n in range(1, num + 1):
+#     sum += n
+#   return sum
+# print(sum_to(6))
+# print(sum_to(10))
+
+
+
+# def largest(nums):
+#   nums.sort()
+#   return nums[-1]
+
+# print(largest([1, 2, 3, 4, 0]))
+# print(largest([10, 4, 2, 231, 91, 54]))
