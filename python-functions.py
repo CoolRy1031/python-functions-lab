f904f4afff0886f98942533f089328f4d7e43bfe
CoolRy <that007guy@hotmail.com>
2022-07-31 13:09:47 -0400

    exercise 4

--- python-functions.py
+++ python-functions.py
@@ -1,16 +1,34 @@
-# def sum_to(num):
-#   sum = 0
-#   for n in range(1, num + 1):
-#     sum += n
-#   return sum
-# print(sum_to(6))
-# print(sum_to(10))
+def sum_to(num):
+  sum = 0
+  for n in range(1, num + 1):
+    sum += n
+  return sum
+print(sum_to(6))
+print(sum_to(10))
 
 
 
-# def largest(nums):
-#   nums.sort()
-#   return nums[-1]
+def largest(nums):
+  nums.sort()
+  return nums[-1]
 
-# print(largest([1, 2, 3, 4, 0]))
-# print(largest([10, 4, 2, 231, 91, 54]))
+print(largest([1, 2, 3, 4, 0]))
+print(largest([10, 4, 2, 231, 91, 54]))
+
+def occurrences(string, string2):
+  return string.count(string2)
+print(occurrences('fleep floop', 'e'))
+print(occurrences('fleep floop', 'p'))
+print(occurrences('fleep floop', 'ee'))
+print(occurrences('fleep floop', 'fe'))
+
+
+def product(*args):
+  product = 1
+  for arg in args:
+    product *= arg
+  return product
+
+print(product(-1, 4))
+print(product(2, 5, 5))
+print(product(4, 0.5, 5))
